9d2cc80c8eaff71e90260295cf276cc112b9c3c6
Rashid Lazytech <lazyt3ch@gmail.com>
2022-11-18 14:01:06 +0500

    add several functions from ris58h/youtube-timestamps into youtubei.js

--- extension/background/youtubei.js
+++ extension/background/youtubei.js
@@ -54,3 +54,75 @@ function parseTimestamp(ts) {
     const hours = parseInt(parts[2]) || 0
     return secs + (60 * mins) + (60 * 60 * hours)
 }
+
+// time-comments related code below
+
+export async function fetchTimeComments(videoId) {
+    const comments = await fetchComments(videoId)
+    const timeComments = []
+    for (const comment of comments) {
+        const tsContexts = getTimestampContexts(comment.text)
+        if (isChaptersComment(tsContexts)) {
+            continue
+        }
+        for (const tsContext of tsContexts) {
+            timeComments.push(newTimeComment(comment.authorAvatar, comment.authorName, tsContext))
+        }
+    }
+    return timeComments
+}
+
+function isChaptersComment(tsContexts) {
+    if (tsContexts.length < 3) {
+        return false
+    }
+    if (tsContexts[0].time !== 0) {
+        return false
+    }
+    return true
+}
+
+async function fetchComments(videoId) {
+    return await youtubei.fetchComments(videoId)
+}
+
+function newTimeComment(authorAvatar, authorName, tsContext) {
+    return {
+        authorAvatar,
+        authorName,
+        timestamp: tsContext.timestamp,
+        time: tsContext.time,
+        text: tsContext.text
+    }
+}
+
+function getTimestampContexts(text) {
+    const result = []
+    const positions = findTimestamps(text)
+    for (const position of positions) {
+        const timestamp = text.substring(position.from, position.to)
+        const time = parseTimestamp(timestamp)
+        if (time === null) {
+            continue
+        }
+        result.push({
+            text,
+            time,
+            timestamp
+        })
+    }
+    return result
+}
+
+function findTimestamps(text) {
+    const result = []
+    const timestampPattern = /(\d?\d:)?(\d?\d:)\d\d/g
+    let match
+    while ((match = timestampPattern.exec(text))) {
+        result.push({
+            from: match.index,
+            to: timestampPattern.lastIndex
+        })
+    }
+    return result
+}
