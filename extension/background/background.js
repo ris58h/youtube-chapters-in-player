9209705cd2dc37add75b17af5340ed545f167da9
Rashid Lazytech <lazyt3ch@gmail.com>
2022-11-18 14:31:41 +0500

    re-add functionality from ris58h/youtube-timestamps into background.js, youtubei.js

--- extension/background/background.js
+++ extension/background/background.js
@@ -8,7 +8,8 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
                 console.error(e)
             })
         return true
-    } else if (request.type == 'fetchTimeComments') {
+    }
+    if (request.type == 'fetchTimeComments') {
         fetchTimeComments(request.videoId)
             .then(sendResponse)
             .catch(e => {
@@ -22,8 +23,80 @@ async function fetchChapters(videoId) {
     return await youtubei.fetchChapters(videoId)
 }
 
+// time-comments related code below
+
 async function fetchTimeComments(videoId) {
     console.log('function fetchTimeComments')
     return await youtubei.fetchTimeComments(videoId)
+}
+
+async function fetchTimeComments(videoId) {
+    const comments = await fetchComments(videoId)
+    const timeComments = []
+    for (const comment of comments) {
+        const tsContexts = getTimestampContexts(comment.text)
+        if (isChaptersComment(tsContexts)) {
+            continue
+        }
+        for (const tsContext of tsContexts) {
+            timeComments.push(newTimeComment(comment.authorAvatar, comment.authorName, tsContext))
+        }
+    }
+    return timeComments
+}
+
+function isChaptersComment(tsContexts) {
+    if (tsContexts.length < 3) {
+        return false
+    }
+    if (tsContexts[0].time !== 0) {
+        return false
+    }
+    return true
+}
+
+async function fetchComments(videoId) {
+    return await youtubei.fetchComments(videoId)
+}
+
+function newTimeComment(authorAvatar, authorName, tsContext) {
+    return {
+        authorAvatar,
+        authorName,
+        timestamp: tsContext.timestamp,
+        time: tsContext.time,
+        text: tsContext.text
+    }
+}
+
+function getTimestampContexts(text) {
+    const result = []
+    const positions = findTimestamps(text)
+    for (const position of positions) {
+        const timestamp = text.substring(position.from, position.to)
+        const time = youtubei.parseTimestamp(timestamp)
+        if (time === null) {
+            continue
+        }
+        result.push({
+            text,
+            time,
+            timestamp
+        })
+    }
+    return result
+}
+
+function findTimestamps(text) {
+    const result = []
+    const timestampPattern = /(\d?\d:)?(\d?\d:)\d\d/g
+    let match
+    while ((match = timestampPattern.exec(text))) {
+        result.push({
+            from: match.index,
+            to: timestampPattern.lastIndex
+        })
+    }
+    return result
 }
 
--- extension/background/youtubei.js
+++ extension/background/youtubei.js
@@ -57,73 +57,102 @@ function parseTimestamp(ts) {
 
 // time-comments related code below
 
-export async function fetchTimeComments(videoId) {
-    const comments = await fetchComments(videoId)
-    console.log('FUNCTION fetchTimeComments :: comments =', comments)
-    const timeComments = []
-    for (const comment of comments) {
-        const tsContexts = getTimestampContexts(comment.text)
-        if (isChaptersComment(tsContexts)) {
-            continue
-        }
-        for (const tsContext of tsContexts) {
-            timeComments.push(newTimeComment(comment.authorAvatar, comment.authorName, tsContext))
-        }
-    }
-    return timeComments
-}
+const MAX_COMMENT_PAGES = 5
+const MAX_COMMENTS = 100
 
-function isChaptersComment(tsContexts) {
-    if (tsContexts.length < 3) {
-        return false
-    }
-    if (tsContexts[0].time !== 0) {
-        return false
-    }
-    return true
-}
-
-async function fetchComments(videoId) {
-    return await fetchComments(videoId)
-}
-
-function newTimeComment(authorAvatar, authorName, tsContext) {
-    return {
-        authorAvatar,
-        authorName,
-        timestamp: tsContext.timestamp,
-        time: tsContext.time,
-        text: tsContext.text
-    }
-}
-
-function getTimestampContexts(text) {
-    const result = []
-    const positions = findTimestamps(text)
-    for (const position of positions) {
-        const timestamp = text.substring(position.from, position.to)
-        const time = parseTimestamp(timestamp)
-        if (time === null) {
-            continue
-        }
-        result.push({
-            text,
-            time,
-            timestamp
+// YouTube declines requests with wrong Origin.
+// We have to remove the Origin header which is added automatically by the browser.
+chrome.webRequest.onBeforeSendHeaders.addListener(
+    details => {
+        const newRequestHeaders = details.requestHeaders.filter(header => {
+            return header.name.toLowerCase() !== "origin"
         })
+        return {requestHeaders: newRequestHeaders}
+    },
+    {urls: ["https://www.youtube.com/*"]},
+    ["blocking", "requestHeaders", chrome.webRequest.OnBeforeSendHeadersOptions.EXTRA_HEADERS].filter(Boolean)
+)
+
+const INNERTUBE_API_KEY = "AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
+const INNERTUBE_CLIENT_NAME = "WEB"
+const INNERTUBE_CLIENT_VERSION = "2.20211129.09.00"
+
+export async function fetchComments(videoId) {
+    const videoResponse = await fetchVideo(videoId)
+    let token = commentsContinuationToken(videoResponse)
+    if (!token) {
+        return []
     }
-    return result
+    const comments = []
+    let prevToken
+    let pageCount = 0
+    while (prevToken !== token && pageCount < MAX_COMMENT_PAGES && comments.length < MAX_COMMENTS) {
+        const commentsResponse = await fetchNext(token)
+        prevToken = token
+        const items = pageCount === 0
+            ? commentsResponse.onResponseReceivedEndpoints[1].reloadContinuationItemsCommand.continuationItems
+            : commentsResponse.onResponseReceivedEndpoints[0].appendContinuationItemsAction.continuationItems
+        if (!items) {
+            break
+        }
+        for (const item of items) {
+            if (item.commentThreadRenderer) {
+                const cr = item.commentThreadRenderer.comment.commentRenderer
+                const authorName = cr.authorText.simpleText
+                const authorAvatar = cr.authorThumbnail.thumbnails[0].url
+                const text = cr.contentText.runs
+                    .map(run => run.text)
+                    .join("")
+                comments.push({
+                    authorName,
+                    authorAvatar,
+                    text
+                })
+            } else if (item.continuationItemRenderer) {
+                token = item.continuationItemRenderer.continuationEndpoint.continuationCommand.token
+            }
+        }
+        pageCount++
+    }
+    return comments
 }
 
-function findTimestamps(text) {
-    const result = []
-    const timestampPattern = /(\d?\d:)?(\d?\d:)\d\d/g
-    let match
-    while ((match = timestampPattern.exec(text))) {
-        result.push({
-            from: match.index,
-            to: timestampPattern.lastIndex
-        })
+function commentsContinuationToken(videoResponse) {
+    return videoResponse.find(e => e.response).response
+        .contents.twoColumnWatchNextResults.results.results
+        .contents.find(e => e.itemSectionRenderer && e.itemSectionRenderer.sectionIdentifier === 'comment-item-section').itemSectionRenderer
+        .contents[0].continuationItemRenderer// When comments are disabled there is messageRenderer instead.
+        ?.continuationEndpoint.continuationCommand.token
+}
+
+async function fetchVideo(videoId) {
+    const response = await fetch(`https://www.youtube.com/watch?v=${videoId}&pbj=1`, {
+        credentials: "omit",
+        headers: {
+            "X-Youtube-Client-Name": "1",
+            "X-Youtube-Client-Version": INNERTUBE_CLIENT_VERSION
+        }
+    })
+    return await response.json()
+}
+
+async function fetchNext(continuation) {
+    const body = {
+        context: {
+            client: {
+                clientName: INNERTUBE_CLIENT_NAME,
+                clientVersion: INNERTUBE_CLIENT_VERSION
+            }
+        },
+        continuation
     }
-    return result
+    const response = await fetch(`https://www.youtube.com/youtubei/v1/next?key=${INNERTUBE_API_KEY}`, {
+        method: "POST",
+        credentials: "omit",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(body)
+    })
+    return await response.json()
 }
