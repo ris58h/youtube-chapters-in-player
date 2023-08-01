41861d8849a4958a63a06ab22032e6b21f8a9764
Ilya Rodionov <ris58h@gmail.com>
2023-08-01 22:53:44 +0300

    dist script

---
+++ dist.js
@@ -0,0 +1,20 @@
+import fs from 'fs'
+import archiver from 'archiver'
+
+if (!fs.existsSync("./dist")) {
+    fs.mkdirSync("./dist")
+}
+
+const name = process.env.npm_package_name
+const version = readJsonSync("./extension/manifest.json").version
+const output = fs.createWriteStream(`./dist/${name}-${version}.zip`)
+const archive = archiver("zip")
+archive.pipe(output)
+archive.glob("**/*", { cwd: "./extension" })
+archive.on("error", err => { throw err })
+archive.finalize()
+
+function readJsonSync(path) {
+    const content = fs.readFileSync(path, "utf8")
+    return JSON.parse(content)
+}
